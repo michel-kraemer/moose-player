687339b222212b547ca6443999d9ced9d067f51d
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2018-10-06 22:50:26 +0200

    Add song database and improve UI

--- index.js
+++ index.js
@@ -1,69 +1,179 @@
 const ansiEscapes = require("ansi-escapes");
+const chalk = require("chalk");
 const fs = require("fs");
+const lunr = require("lunr");
+const moment = require("moment");
 const NativePlayer = require("native_player");
 const path = require("path");
 const readline = require("readline");
 
-if (process.argc < 2) {
-  console.error("Usage: node index.js [directory]");
+if (process.argv.length < 3) {
+  console.error("Usage: node index.js [query]");
   process.exit(1);
 }
 
-let dir = process.argv[2];
-let files = fs.readdirSync(dir, { withFileTypes: true });
-files = files.filter(f => {
-  return f.isFile() && (
-    f.name.toLowerCase().endsWith(".aac") ||
-    f.name.toLowerCase().endsWith(".m4a") ||
-    f.name.toLowerCase().endsWith(".mp3") ||
-    f.name.toLowerCase().endsWith(".ogg") ||
-    f.name.toLowerCase().endsWith(".wav")
-  );
+// load database
+let albums = {};
+let database = JSON.parse(fs.readFileSync("database.json"));
+let covers = database.covers;
+let index = lunr(function() {
+  this.field("artist");
+  this.field("album");
+  this.field("title");
+  this.ref("ref");
+  database.index.forEach(track => {
+    track.ref = track.artist + " - " + track.album;
+    if (!albums[track.ref]) {
+      albums[track.ref] = [];
+    }
+    albums[track.ref].push(track);
+    this.add(track);
+  });
 });
-files = files.map(f => f.name);
 
+// search for album to play
+let searchresults = index.search(process.argv[2]);
+if (searchresults.length == 0) {
+  console.log("Found no albums");
+  process.exit(0);
+} else if (searchresults.length > 1) {
+  console.log("Found multiple albums:");
+  console.log();
+  searchresults.forEach(r => console.log("- " + r.ref));
+  console.log();
+  console.log("Please narrow your search");
+  process.exit(0);
+}
+
+// sort album tracks
+let album = albums[searchresults[0].ref];
+album.sort((a, b) => {
+  if (a.track && b.track) {
+    return a.track - b.track;
+  }
+  return a.title.localeCompare(b.title);
+});
+
+// initialize player
 let player = new NativePlayer();
 player.init(2, 48000);
 player.play();
-for (let f of files) {
-  player.queue(path.join(dir, f));
+for (let track of album) {
+  player.queue(track.path);
 }
 
-let coverPath = path.join(dir, "cover.jpg");
-if (!fs.existsSync(coverPath)) {
-  coverPath = "cover.jpg";
-}
-let cover = fs.readFileSync(coverPath);
-let coverAnsi = ansiEscapes.image(cover, {
-  width: 20
-});
-
-// make some room for our UI
+// make some room for the UI
 for (let i = 0; i < 11; ++i) {
   console.log();
 }
-process.stdout.write(ansiEscapes.cursorSavePosition);
+process.stdout.write(ansiEscapes.cursorSavePosition + ansiEscapes.cursorHide);
 
-// display cover
-let s = ansiEscapes.cursorHide +
-  ansiEscapes.cursorUp(10) +
-  coverAnsi +
-  ansiEscapes.cursorRestorePosition;
-process.stdout.write(s);
+function findTrack(path) {
+  for (let track of album) {
+    if (track.path === path) {
+      return track;
+    }
+  }
+  return undefined;
+}
 
-// run timer that displays title information
-let timerId = setInterval(() => {
-  let title = "";
-  let song = player.currentSong();
-  if (song) {
-    title = path.basename(song.path, path.extname(song.path));
+function showCover(track) {
+  let cover;
+  if (track.cover) {
+    cover = Buffer.from(covers[track.cover], "base64");
+  } else {
+    let coverpath = path.join(path.dirname(track.path), "cover.jpg");
+    if (fs.existsSync(coverpath)) {
+      cover = fs.readFileSync(coverpath);
+    }
   }
 
-  let s = ansiEscapes.cursorMove(21, -10) +
-    ansiEscapes.eraseEndLine +
-    title +
+  if (cover) {
+    let s = ansiEscapes.cursorMove(0, -10) +
+      ansiEscapes.image(cover, { width: 20 }) +
+      ansiEscapes.cursorRestorePosition;
+    process.stdout.write(s);
+  }
+}
+
+// run timer that displays title information
+let currentSongPath;
+let currentSongStartTime = moment();
+let timerId = setInterval(() => {
+  let song = player.currentSong();
+  if (song && currentSongPath != song.path) {
+    currentSongPath = song.path;
+    currentSongStartTime = moment();
+
+    let artist = "";
+    let album_name = "";
+    let title = "";
+    let duration = "";
+    let album_info = "";
+
+    let track = findTrack(currentSongPath);
+    if (track) {
+      showCover(track);
+      artist = track.artist;
+      album_name = track.album;
+      title = track.title;
+      if (track.track) {
+        title = track.track + ". " + title;
+      }
+      if (track.year) {
+        album_name = album_name + " (" + track.year + ")";
+      }
+      if (track.duration) {
+        duration = "/ " + moment(track.duration * 1000).format("mm:ss");
+      }
+      album_info = album.length + " songs";
+      let album_duration = 0;
+      for (let t of album) {
+        if (t.duration) {
+          album_duration += t.duration;
+        } else {
+          album_duration = 0;
+          break;
+        }
+      }
+      if (album_duration > 0) {
+        album_info = album_info + " (" + moment(album_duration * 1000).format("mm:ss") + ")";
+      }
+    }
+
+    let infostr = ansiEscapes.cursorMove(21, -10) +
+      ansiEscapes.eraseEndLine +
+      album_name +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorMove(21) +
+      ansiEscapes.eraseEndLine +
+      chalk.gray(artist) +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorMove(21) +
+      ansiEscapes.eraseEndLine +
+      chalk.bold(title) +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorMove(21) +
+      ansiEscapes.eraseEndLine +
+      ansiEscapes.cursorMove(6) +
+      duration +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorNextLine +
+      ansiEscapes.cursorMove(21) +
+      ansiEscapes.eraseEndLine +
+      chalk.gray(album_info) +
+      ansiEscapes.cursorRestorePosition;
+    process.stdout.write(infostr);
+  }
+
+  let duration = moment(moment().diff(currentSongStartTime));
+  let timestr = ansiEscapes.cursorMove(21, -4) +
+    duration.format("mm:ss") +
     ansiEscapes.cursorRestorePosition;
-  process.stdout.write(s);
+  process.stdout.write(timestr);
 }, 500);
 
 // handle keypress events
---
+++ reindex.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const mm = require("music-metadata");
+const path = require("path");
+
+if (process.argv.length < 3) {
+  console.error("Usage: node reindex.js [directory]");
+  process.exit(1);
+}
+
+let covers = [];
+let dir = process.argv[2];
+(async () => {
+  let index = await reindex(dir);
+  let result = {
+    index,
+    covers
+  };
+  fs.writeFileSync("database.json", JSON.stringify(result, undefined, 2));
+})();
+
+function makeCover(cover) {
+  let i = covers.indexOf(cover);
+  if (i == -1) {
+    i = covers.length;
+    covers.push(cover);
+  }
+  return i;
+}
+
+async function reindex(dir) {
+  let result = [];
+  let files = fs.readdirSync(dir, { withFileTypes: true });
+  for (let f of files) {
+    let absolutePath = path.join(dir, f.name);
+    if (f.isDirectory()) {
+      result = result.concat(await reindex(absolutePath));
+    } else {
+      try {
+        let metadata = await mm.parseFile(absolutePath);
+        let cover = undefined;
+        if (metadata.common.picture) {
+          cover = makeCover(metadata.common.picture[0].data.toString("base64"));
+        }
+        let track = undefined;
+        if (metadata.common.track && metadata.common.track.no) {
+          track = metadata.common.track.no;
+        }
+        let simple = {
+          artist: metadata.common.artist,
+          album: metadata.common.album,
+          track,
+          title: metadata.common.title,
+          year: metadata.common.year,
+          cover,
+          duration: metadata.format.duration,
+          path: absolutePath
+        };
+        result.push(simple);
+      } catch (e) {
+        console.log(absolutePath);
+        console.warn(e.message);
+      }
+    }
+  }
+  return result;
+}
