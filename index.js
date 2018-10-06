c710eaac3ed0fc2a1abe2cbc84f208b96af69713
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2018-10-06 19:35:27 +0200

    Play all files from a directory

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const ansiEscapes = require("ansi-escapes");
 const fs = require("fs");
 const NativePlayer = require("native_player");
+const path = require("path");
 const readline = require("readline");
+
+if (process.argc < 2) {
+  console.error("Usage: node index.js [directory]");
+  process.exit(1);
+}
+
+let dir = process.argv[2];
+let files = fs.readdirSync(dir, { withFileTypes: true });
+files = files.filter(f => {
+  return f.isFile() && (
+    f.name.toLowerCase().endsWith(".aac") ||
+    f.name.toLowerCase().endsWith(".m4a") ||
+    f.name.toLowerCase().endsWith(".mp3") ||
+    f.name.toLowerCase().endsWith(".ogg") ||
+    f.name.toLowerCase().endsWith(".wav")
+  );
+});
+files = files.map(f => f.name);
 
 let player = new NativePlayer();
 player.init(2, 48000);
 player.play();
-player.queue("lisa0.mp3");
-player.queue("short.mp3");
-// player.queue("Mixdown(2).mp3");
+for (let f of files) {
+  player.queue(path.join(dir, f));
+}
 
-let cover = fs.readFileSync("cover.jpg");
+let coverPath = path.join(dir, "cover.jpg");
+if (!fs.existsSync(coverPath)) {
+  coverPath = "cover.jpg";
+}
+let cover = fs.readFileSync(coverPath);
 let coverAnsi = ansiEscapes.image(cover, {
   width: 20
 });
@@ -33,10 +56,10 @@ let timerId = setInterval(() => {
   let title = "";
   let song = player.currentSong();
   if (song) {
-    title = song.path;
+    title = path.basename(song.path, path.extname(song.path));
   }
 
-  let s = ansiEscapes.cursorMove(22, -10) +
+  let s = ansiEscapes.cursorMove(21, -10) +
     ansiEscapes.eraseEndLine +
     title +
     ansiEscapes.cursorRestorePosition;
